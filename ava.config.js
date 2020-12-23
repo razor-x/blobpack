a28190bd83ad9b48cc9bdc49761a2e13918f8733
Evan Sosenko <razorx@evansosenko.com>
2020-12-23 10:53:40 -0600

    Use @ava/babel over @babel/register

--- ava.config.js
+++ ava.config.js
@@ -1,9 +1,11 @@
+export default () => ({
+  files: ['**/*.spec.js', '!dist/**/*', '!package/**/*'],
+  ...isBabelRequired() ? { babel } : {}
+})
+
 const isBabelRequired = () => {
   const [majorVer] = process.versions.node.split()
   return Number(majorVer) < 14
 }
 
-export default {
-  files: ['**/*.spec.js', '!dist/**/*', '!package/**/*'],
-  require: isBabelRequired() ? ['@babel/register'] : undefined
-}
+const babel = true
